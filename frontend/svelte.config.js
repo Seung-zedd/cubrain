66e7631b3044db479af241fa3c2222119f643499
michael Jo <csz0209@gmail.com>
2025-12-04 16:54:39 +0900

    ⚙️chore: change adapter feature to detect the Vercel environment and configure the build output correctly

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 import { fileURLToPath } from "url";
@@ -28,9 +28,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      fallback: "index.html",
-    }),
+    adapter: adapter(),
   },
 };
 
