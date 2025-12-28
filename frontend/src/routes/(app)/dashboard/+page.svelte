90e49520a3e944bedf796ddc6ee553785a013451
michael Jo <csz0209@gmail.com>
2025-12-28 17:43:10 +0900

    ⚙️chore: change client headers to application/json to match backend file type expectation

--- frontend/src/routes/(app)/dashboard/+page.svelte
+++ frontend/src/routes/(app)/dashboard/+page.svelte
@@ -271,6 +271,9 @@
     try {
       const response = await authFetch("/api/v1/decks", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           title:
             jobMetadata.title ||
